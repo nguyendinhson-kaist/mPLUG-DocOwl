fbb7353167860c8570231377539ecee25b337f18
nguyendinhson-kaist <nguyendinhson@kaist.ac.kr>
2024-10-06 17:37:47 +0900

    add script for slurm training

--- DocOwl1.5/scripts/finetune_docowl_instructdoc_lora.sh
+++ DocOwl1.5/scripts/finetune_docowl_instructdoc_lora.sh
@@ -19,26 +19,29 @@
 # echo $DISTRIBUTED_ARGS
 
 # change LOAD to your local path of DocOwl1.5-stage1
-LOAD='mPLUG/DocOwl1.5-stage1'
+LOAD='mPLUG/DocOwl1.5-Omni'
+
+OUTPUT_DIR=/scratch/slurm-user23-kaist/son/code/mPLUG-DocOwl/DocOwl1.5/checkpoints/docowl1.5-omni-instrucdoc-lora-1e4-azure
 
 # batch size = per_device_train_batch_size x GPUS_PER_NODE x NNODES x gradient_accumulation_steps
-DATA_FILE=/mnt/KAIST/son/VD-Instruct/data/vd-instruct/train/no_ocr/instructdoc_docowl_train_5k.jsonl
+DATA_FILE=/scratch/slurm-user23-kaist/son/code/VD-Instruct/data/vd-instruct-2/train/azure/instructdoc_docowl_train_5k.jsonl
+
 # CUDA_VISIBLE_DEVICES=3 torchrun mplug_docowl/train/train_docowl.py \
-deepspeed -i localhost:3 mplug_docowl/train/train_docowl.py \
+deepspeed /scratch/slurm-user23-kaist/son/code/mPLUG-DocOwl/DocOwl1.5/mplug_docowl/train/train_docowl.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --vision2text_lr 2e-5 \
-    --deepspeed ./scripts/zero2.json \
+    --deepspeed /scratch/slurm-user23-kaist/son/code/mPLUG-DocOwl/DocOwl1.5/scripts/zero2.json \
     --model_name_or_path $LOAD \
-    --cache_dir ./checkpoints \
+    --cache_dir /scratch/slurm-user23-kaist/son/code/mPLUG-DocOwl/DocOwl1.5/checkpoints \
     --version v1 \
     --data_path $DATA_FILE \
-    --image_folder /mnt/KAIST/dao/instructdocs/raw_datasets \
+    --image_folder /scratch/slurm-user23-kaist/son/data/instructdocs/raw_datasets \
     --image_size 448 \
     --crop_anchors 'grid_9' \
     --add_global_img True \
     --add_textual_crop_indicator True \
     --bf16 True \
-    --output_dir ./checkpoints/docowl1.5-lora \
-    --num_train_epochs 3 \
+    --output_dir $OUTPUT_DIR \
+    --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 8 \
